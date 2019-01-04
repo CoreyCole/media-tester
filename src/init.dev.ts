0b2835c428863c8b9de71294a11490ca4c312451
coreycole <coreyleoc@gmail.com>
2019-01-03 16:58:20 -0800

    remove unused dependecy

--- src/init.dev.ts
+++ src/init.dev.ts
@@ -1,4 +1,4 @@
 import * as bst from 'bespoken-tools';
 
 const server = new bst.LambdaServer('./src/index', 10001, true);
-server.start(() => console.log(yellow('[init.dev]: server started and listening on port 10001!')));
+server.start(() => console.log('[init.dev]: server started and listening on port 10001!'));
